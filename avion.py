e1b1bcaaddaf16d96c05f7d111670fe3f7f01361
lapequemochi <lapequemochi7@gmail.com>
2024-05-02 16:10:14 -0500

    xdes

--- avion.py
+++ avion.py
@@ -17,16 +17,16 @@ class Avion:
     def definicionSillasEjecutivas(self):
         for i in range (self.SILLAS_EJECUTIVAS):
             if (i+1)%2 == 0:
-                self.sillasEjecutivas.append ((i+1) 1, Silla.CLASE.eje, Silla.UBICACION.ventana)
+                self.sillasEjecutivas.append((i+1) 1, Silla.CLASE.eje, Silla.UBICACION.ventana)
             else:
-                self.sillasEjecutivas.append ((i+1) 2, Silla.CLASE.eje, Silla.UBICACION.pasillo)
+                self.sillasEjecutivas.append((i+1) 2, Silla.CLASE.eje, Silla.UBICACION.pasillo)
                 
     def definicionSillasEConomicas(self):
         for i in range (self.SILLAS_ECONOMICAS):
-            if (i+1)%3 == 0:
-                self.sillasEconomicas.append ((i+1)1, Silla.CLASE.eco, Silla.UBICACION.ventana)
-                
-                self.sillasEjecutivas.append ((i+1)2, Silla.CLASE.eco, Silla.UBICACION.pasillo)
+            if (i+1)%2 == 0:
+                self.sillasEjecutivas.append((i + 1, Silla.CLASE.eco, Silla.UBICACION.ventana))
+                self.sillasEjecutivas.append((i + 1, Silla.CLASE.eco, Silla.UBICACION.pasillo))
             else:
-                self.sillasEjecutivas.append ((i+1)3, Silla.CLASE.eco, Silla.UBICACION.centro)
-                
+                self.sillasEjecutivas.append((i + 1, Silla.CLASE.eco, Silla.UBICACION.centro))
+            
+    
