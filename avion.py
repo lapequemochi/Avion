4a93118463a8ba7b26002e055e631214204a1ee8
lapequemochi <lapequemochi7@gmail.com>
2024-05-02 08:40:16 -0500

    XDE

--- avion.py
+++ avion.py
@@ -6,4 +6,12 @@ class Avion:
         self.sillasEjecutivas = []
         self.sillasEconomicas = []
         
+    def __init__ (self):
+        self.sillasEjecutivas =[]
+        self.sillasEconomicas =[]
     
+        self.sillasEjecutivas.append(1, Silla.CLASE.eje, Silla.UBICACION.ventana)
+        self.sillasEjecutivas.append(2, Silla.CLASE.eje, Silla.UBICACION.pasillo)
+    
+    def definicionSillasEjecutivas(self):
+        for i in range (self.SILLAS_EJECUTIVAS):
