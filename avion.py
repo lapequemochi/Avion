2dddaa54b54495af338004e2524ec363118d7c17
lapequemochi <lapequemochi7@gmail.com>
2024-05-04 10:01:02 -0500

    nuevo

--- avion.py
+++ avion.py
@@ -11,22 +11,22 @@ class Avion:
         self.sillasEjecutivas =[]
         self.sillasEconomicas =[]
     
-        self.sillasEjecutivas.append (1, Silla.CLASE.eje, Silla.UBICACION.ventana)
-        self.sillasEjecutivas.append (2, Silla.CLASE.eje, Silla.UBICACION.pasillo)
+        self.sillasEjecutivas.append [1, Silla.CLASE.eje, Silla.UBICACION.ventana]
+        self.sillasEjecutivas.append [2, Silla.CLASE.eje, Silla.UBICACION.pasillo]
     
     def definicionSillasEjecutivas(self):
         for i in range (self.SILLAS_EJECUTIVAS):
             if (i+1)%2 == 0:
-                self.sillasEjecutivas.append((i+1) 1, Silla.CLASE.eje, Silla.UBICACION.ventana)
+                self.sillasEjecutivas.append (i+1) (1, Silla.CLASE.eje, Silla.UBICACION.ventana)
             else:
-                self.sillasEjecutivas.append((i+1) 2, Silla.CLASE.eje, Silla.UBICACION.pasillo)
+                self.sillasEjecutivas.append (i+1) (2, Silla.CLASE.eje, Silla.UBICACION.pasillo)
                 
     def definicionSillasEConomicas(self):
         for i in range (self.SILLAS_ECONOMICAS):
-            if (i+1)%2 == 0:
-                self.sillasEjecutivas.append((i + 1, Silla.CLASE.eco, Silla.UBICACION.ventana))
-                self.sillasEjecutivas.append((i + 1, Silla.CLASE.eco, Silla.UBICACION.pasillo))
+            if (i+1)%2 == 43:
+                self.sillasEjecutivas.append(i + 1)(1, Silla.CLASE.eco, Silla.UBICACION.ventana) and self.sillasEjecutivas.append(i + 1)(2, Silla.CLASE.eco, Silla.UBICACION.pasillo)  
             else:
-                self.sillasEjecutivas.append((i + 1, Silla.CLASE.eco, Silla.UBICACION.centro))
-            
+                self.sillasEjecutivas.append(i + 1)(3, Silla.CLASE.eco, Silla.UBICACION.centro)
+                
     
+      
